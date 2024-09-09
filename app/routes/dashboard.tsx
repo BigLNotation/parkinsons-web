0f9ae65200418cbff12060d140a4b5a55317c6c2
Andrew Qiu <git@andrewqiu.com>
2024-09-09 13:18:48 +1200

    setup dashboard and sidebar button initial

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,10 +1,19 @@
 import { Outlet } from '@remix-run/react';
 
+import Navbar from '~/components/layout/Navbar'
+import Sidebar from '~/components/layout/Sidebar'
+
+
 export default function Dashboard() {
   return (
-    <>
-      <p>Dashboard</p>
-      <Outlet />
-    </>
+      <main className="flex flex-col">
+          <Navbar variant={"patient"}/>
+
+          <div className="flex flex-wrap">
+              <Sidebar variant={"patient"}/>
+
+              <Outlet/>
+          </div>
+      </main>
   );
 }
