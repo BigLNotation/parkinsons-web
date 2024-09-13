71496394ab67b8e0ecc0b58b1cde3533930a2237
Andrew Qiu <git@andrewqiu.com>
2024-09-14 02:17:35 +1200

    add full size option for buttons and dash modules

--- app/components/dashboards/DashboardModule.tsx
+++ app/components/dashboards/DashboardModule.tsx
@@ -4,8 +4,15 @@ the boxes for layouts on logged-in pages
 
 import React from "react";
 
+/*
+PROPS:
+Variant: the color of the box. Normal = gray, accent = purple.
+isFullSize: Make the module the full size of the parent element.
+ */
+
 type DashboardProps = {
     variant: "normal" | "accent",
+    isFullSize?: boolean,
     children: string | React.ReactNode,
 }
 
@@ -14,9 +21,9 @@ const variants = {
     accent: "bg-purple-980 border-2 border-purple-850 py-8 px-9 rounded-2xl w-full"
 }
 
-function DashboardModule({variant, children}: DashboardProps) {
+function DashboardModule({variant, isFullSize = false, children}: DashboardProps) {
     return(
-        <div className={variants[variant]}>
+        <div className={`${variants[variant]} ${isFullSize && "h-full w-full"}`}>
             {children}
         </div>
     )
